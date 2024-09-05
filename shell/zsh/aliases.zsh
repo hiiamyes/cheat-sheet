82300292a0426a710cc229fbe4c6ad684166f1f8
Yes Lee <joshuayes@gmail.com>
2024-09-05 12:04:21 +0800

    wip

---
+++ shell/zsh/aliases.zsh
@@ -0,0 +1,31 @@
+# # at
+# alias at-etl="code ~/codes/at/amazing-etl"
+# alias at-internal-services="code ~/codes/at/at-internal-services"
+# alias at-hiring="code ~/Dropbox/at/hiring"
+# alias at-staging-server="ssh deploy@13.112.77.68"
+# alias at-pwd="code ~/Dropbox/at/pwd.md"
+# alias nuxt="code ~/codes/at/nuxt"
+# alias nuxt-flash="code ~/codes/at/nuxt-flash"
+# alias i18n-nuxt="code ~/codes/at/i18n-nuxt"
+# alias rails="code ~/codes/at/docker-docker/rails"
+# alias at-i18n-ec2='cd ~/Dropbox/at && ssh -i "infra-tools.pem" ubuntu@ec2-13-113-219-161.ap-northeast-1.compute.amazonaws.com'
+# alias at-redis="redis-cli -h v2-gd-edge-tokyo.ivvdca.ng.0001.apne1.cache.amazonaws.com"
+
+# wordup
+alias portal="code ~/codes/wordup/portal"
+alias shop="code ~/codes/wordup/shop2"
+alias app="code ~/codes/wordup/app"
+alias components="code ~/codes/wordup/components"
+
+# yes
+alias hiking-data="code ~/Dropbox/Yes/hiking/hiking-data.js"
+alias hiiiike="code ~/codes/yes/hiiiike"
+alias paaaack="cd ~/codes/yes/paaaack && code ."
+alias yesleeme="code ~/codes/yes/yeslee.me"
+alias practices="cd ~/codes/yes/practices && code ."
+alias interview="code ~/Dropbox/Yes/doc/interview"
+alias body="code ~/Dropbox/Yes/body-info-size-chart-身體素質.md"
+alias oh-my-zsh="code ~/.oh-my-zsh/custom/aliases.zsh"
+alias yes-linode="ssh yes@139.162.47.241"
+alias secret="code /Users/yes/Dropbox/Yes/new/secret.md && code /Users/yes/Dropbox/Yes/new/secret.yaml"
+alias cheat-sheet="code ~/codes/yes/cheat-sheet"
